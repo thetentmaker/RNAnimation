df3f3ead2b2ce7e5c8889e55cae41b9a50dd2f0a
tentmaker <ovsoc@daum.net>
2025-10-28 14:30:42 +0900

    feat: playlist anim

--- src/youtubeMusic/components/playlist/Playlist.tsx
+++ src/youtubeMusic/components/playlist/Playlist.tsx
@@ -3,6 +3,7 @@ import {
   Image,
   PanResponder,
   StyleSheet,
+  Text,
   useWindowDimensions,
   View,
 } from 'react-native';
@@ -70,7 +71,6 @@ const Playlist = ({ playlistAnim }: PlaylistProps) => {
         {...panResponder.panHandlers}
         style={{
           borderWidth: 1,
-          borderColor: 'red',
           backgroundColor: '#222',
           borderBottomColor: '#666',
           borderBottomWidth: 1,
@@ -90,23 +90,73 @@ const Playlist = ({ playlistAnim }: PlaylistProps) => {
           }),
         }}
       >
-        <Animated.View
-          style={{
-            width: playlistAnim.interpolate({
-              inputRange: [0, height / 2, height],
-              outputRange: [50, width * 0.8, width * 0.8],
-            }),
-            height: playlistAnim.interpolate({
-              inputRange: [0, height / 2, height],
-              outputRange: [50, width * 0.8, width * 0.8],
-            }),
-          }}
-        >
-          <Image
-            source={{ uri: 'https://picsum.photos/50' }}
-            style={styles.image}
-          />
-        </Animated.View>
+        <View>
+          {/* top */}
+          <Animated.View
+            style={{
+              height: playlistAnim.interpolate({
+                inputRange: [height / 2, height],
+                outputRange: [0, 100],
+              }),
+              opacity: playlistAnim.interpolate({
+                inputRange: [height / 2, height],
+                outputRange: [0, 1],
+              }),
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                height: '100%',
+                borderWidth: 1,
+                borderColor: 'white',
+              }}
+            >
+              Top
+            </Text>
+          </Animated.View>
+          {/* image: thumbnail */}
+          <Animated.View
+            style={{
+              width: playlistAnim.interpolate({
+                inputRange: [0, height / 2, height],
+                outputRange: [50, width * 0.8, width * 0.8],
+              }),
+              height: playlistAnim.interpolate({
+                inputRange: [0, height / 2, height],
+                outputRange: [50, width * 0.8, width * 0.8],
+              }),
+            }}
+          >
+            <Image
+              source={{ uri: 'https://picsum.photos/50' }}
+              style={styles.image}
+            />
+          </Animated.View>
+          {/* middle */}
+          <Animated.View
+            style={{
+              height: playlistAnim.interpolate({
+                inputRange: [height / 2, height],
+                outputRange: [0, 250],
+              }),
+              opacity: playlistAnim.interpolate({
+                inputRange: [height / 2, height],
+                outputRange: [0, 1],
+              }),
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                height: '100%',
+                borderColor: 'white',
+              }}
+            >
+              Middle
+            </Text>
+          </Animated.View>
+        </View>
         {/* <PlaylistFull /> */}
         <Animated.View
           style={[
@@ -120,6 +170,25 @@ const Playlist = ({ playlistAnim }: PlaylistProps) => {
           ]}
         >
           <PlaylistMini />
+        </Animated.View>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            width,
+            height: playlistAnim.interpolate({
+              inputRange: [height / 2, height],
+              outputRange: [0, 100],
+            }),
+            borderWidth: 1,
+            borderColor: 'white',
+            bottom: 0,
+            opacity: playlistAnim.interpolate({
+              inputRange: [height / 2, height],
+              outputRange: [0, 1],
+            }),
+          }}
+        >
+          <Text style={{ color: 'white' }}>Bottom</Text>
         </Animated.View>
       </Animated.View>
     </>
